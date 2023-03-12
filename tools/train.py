28de5791a4bb288d2fad2764deb4e784b89d3bd0
hongyong666 <1262085026@qq.com>
2023-03-12 16:43:10 +0800

    去掉了print打印

--- tools/train.py
+++ tools/train.py
@@ -111,7 +111,7 @@ def main():
     args = parse_args()
 
     cfg = Config.fromfile(args.config)
-    print(cfg)
+    # print(cfg)
     # exit()
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
